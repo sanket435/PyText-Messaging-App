5f89111ae63e9fadf1ee0771e7fba53adc43656a
sanket435 <38597527+sanket435@users.noreply.github.com>
2018-04-22 20:11:32 +0530

    Add files via upload

---
+++ sent_msg.py
@@ -0,0 +1,17 @@
+#Using Twilio API
+
+from twilio.rest import Client
+
+# MY Account SID from twililo
+account_sid = "AC1ef0626a958aa2fde91d95fd493f8f62"
+# MY Auth Token from twilio
+auth_token  = "aab0cc15f54f3653a59033df5bec3932"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+919970919048", 
+    from_="+12512208054",
+    body="hey hello there")
+
+print(message.sid)
